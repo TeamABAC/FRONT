5bfac7811dc79b51da422199e9ff28859450dbd6
seohyeon <s24005@gsm.hs.kr>
2024-07-18 08:32:40 +0900

    커밋 성공

---
+++ 메인/메인.js
@@ -0,0 +1,14 @@
+function goTomainpage() {
+  window.location.href =
+    'http://127.0.0.1:5500/%EB%A9%94%EC%9D%B8/%EB%A9%94%EC%9D%B8.html';
+}
+
+function goTosinmungopage() {
+  window.location.href =
+    'http://127.0.0.1:5500/%EB%A9%94%EC%9D%B8/%EB%A9%94%EC%9D%B8.html';
+}
+
+function gotointroducepage() {
+  window.location.href =
+    'http://127.0.0.1:5500/%EB%A9%94%EC%9D%B8/%EB%A9%94%EC%9D%B8.html';
+}
