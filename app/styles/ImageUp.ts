8009a88b39236b397629725154c8a5e33986042b
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2024-11-06 11:20:44 +0900

    주석 제거

--- app/styles/ImageUp.ts
+++ app/styles/ImageUp.ts
@@ -1,3 +1,4 @@
+// styles/ImageUp.js
 import styled from "styled-components";
 
 export const ImageDiv = styled.div`
@@ -5,34 +6,38 @@ export const ImageDiv = styled.div`
   width: 900px;
   height: 50px;
   top: 1500px;
-
   background-color: black;
+  display: flex; /* 플렉스 컨테이너 설정 */
+  align-items: center; /* 수직 중앙 정렬 */
 `;
 
 export const FileInput = styled.input`
   display: none;
   position: absolute;
   left: 20px;
-  heght: 40px;
-
-  & + label {
-    display: inline-block;
-    padding: 10px 20px;
-    background-color: #003CB2;
-    color: white;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-
-    &:hover {
-      background-color: #002b8a;
-    }
-  }
+  height: 40px; /* 파일 입력 높이 */
 `;
 
 export const FileInputLabel = styled.label`
- font-family: 'Pretendard';
+  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 600;
   font-size: 20px;
-` 
+  margin-left: 20px; /* 왼쪽 여백 */
+  cursor: pointer; /* 커서 포인터 */
+  padding: 10px 20px; /* 패딩 */
+  background-color: #003CB2; /* 배경색 */
+  color: white;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #002b8a;
+  }
+`;
+
+export const FileName = styled.span`
+  margin-left: 60px;
+  color: black;
+  font-size: 16px;
+`;
