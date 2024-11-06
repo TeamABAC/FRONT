e83c958da75ef15cf25e0be843b862bb548663bc
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2024-11-06 11:22:53 +0900

    background 지정

--- app/styles/ImageUp.ts
+++ app/styles/ImageUp.ts
@@ -6,16 +6,15 @@ export const ImageDiv = styled.div`
   width: 900px;
   height: 50px;
   top: 1500px;
-  background-color: black;
-  display: flex; /* 플렉스 컨테이너 설정 */
-  align-items: center; /* 수직 중앙 정렬 */
+  background-color: #F4F4F4;
+  display: flex;
+  align-items: center; 
 `;
 
 export const FileInput = styled.input`
   display: none;
   position: absolute;
-  left: 20px;
-  height: 40px; /* 파일 입력 높이 */
+  height: 40px;
 `;
 
 export const FileInputLabel = styled.label`
@@ -23,10 +22,9 @@ export const FileInputLabel = styled.label`
   font-style: normal;
   font-weight: 600;
   font-size: 20px;
-  margin-left: 20px; /* 왼쪽 여백 */
-  cursor: pointer; /* 커서 포인터 */
-  padding: 10px 20px; /* 패딩 */
-  background-color: #003CB2; /* 배경색 */
+  cursor: pointer;
+  padding: 10px 20px;
+  background-color: #003CB2;
   color: white;
   border-radius: 5px;
   transition: background-color 0.3s;
