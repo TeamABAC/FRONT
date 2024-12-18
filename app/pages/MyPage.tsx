7fe89549cad5d35180adaa45c73de21f6e06d32f
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2024-12-18 17:01:45 +0900

    pull request

--- app/pages/MyPage.tsx
+++ app/pages/MyPage.tsx
@@ -6,24 +6,21 @@ import * as S from '../styles/MyPageCss';
 
 function Mypage() {
 
-  const [areSelected ,isSelected ]= useState("");
-  const [selected, setSelected] = useState<string>('마이페이지');
-
-  function userSelect(item : any) {
-    setSelected(item);
-  }
-
   return (
     <>
       <NavBar />
+      <S.MyPageAllBodyDiv> 
 
-      <S.MyPageBodyDiv>
         <S.MyPageTextDiv>
-          <S.MyPageText>
-            마이페이지
-          </S.MyPageText>
+          <S.MyPageText>마이페이지</S.MyPageText>
         </S.MyPageTextDiv>
-      </S.MyPageBodyDiv>
+
+        <S.nameContainer>
+        </S.nameContainer>
+        <br></br>
+        <S.classMailContainer></S.classMailContainer>
+
+      </S.MyPageAllBodyDiv>
     </>
   );
 }
--- app/styles/MyPageCss.ts
+++ app/styles/MyPageCss.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
-export const MyPageBodyDiv = styled.div`
+export const MyPageAllBodyDiv = styled.div`
   width: 100%;
   height: 900px;
   background: linear-gradient(180deg, #6092f2 0%, #295f9f 100%);
+  display:flex;
+  justify-content: center;
 `;
 
 export const MyPageTextDiv = styled.div`
@@ -11,8 +13,8 @@ export const MyPageTextDiv = styled.div`
   position: absolute;
   width: 220px;
   height: 80px;
-  left: 98px;
-  top: 152px;
+  left: 70px;
+  top: 120px;
   background: #ffffff;
   border: 1px solid #a9bad1;
   border-radius: 16px;
@@ -26,10 +28,32 @@ export const MyPageText = styled.span`
   font-family: 'Pretendard';
   font-style: normal;
   font-weight: 700;
-  font-size: 48px;
+  font-size: 40px;
   line-height: 70px;
   color: #4D80D5;
   user-select: none;
 `;
 
 
+export const nameContainer = styled.div`
+  background-color: white;
+  width: 900px;
+  height: 140px;
+  border-radius: 40px;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin-top: 160px;
+  margin-left: 140px;
+`
+
+export const classMailContainer = styled.div`
+  background-color:white;
+  width: 900px;
+  height: 300px;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin-top: 20px;
+  margin-left: 140px;
+`
