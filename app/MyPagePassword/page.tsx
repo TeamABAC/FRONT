934d6d52666bdf18523a5207dc965462856d2570
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2024-12-26 09:30:25 +0900

    새로

---
+++ app/MyPagePassword/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import MyPagePassword from '../pages/MypaPassword';
+
+const goToMyPagePassword: React.FC = () => {
+  return <MyPagePassword />;
+};
+
+export default goToMyPagePassword;
