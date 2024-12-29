f4727f695750bd9865616eb05c91635eb1318399
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2024-12-30 08:14:44 +0900

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-
-
 import Main from './pages/Main';
-import Mypage from './pages/MypaPassword'
-import Seegunhe from './pages/DoorClothLook';
-import Check from './pages/Chekoutgunhe'
-import Result from './pages/ResultGunhe'
 const HomePage: React.FC = () => {
-  return <Result />;
+  return <Main />;
 };
 
 export default HomePage;
