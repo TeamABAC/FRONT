8da5b744bbd90ca61cba3db9e8239e99d960509b
박서현 <s24005@gsm.hs.kr>
2024-12-30 10:28:20 +0900

    a

---
+++ app/Chekoutgunhe/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import Chekoutgunhe from '../pages/Chekoutgunhe'
+
+const CheckoutgunhePage: React.FC = () => {
+  return <Chekoutgunhe />;
+};
+
+export default CheckoutgunhePage;
