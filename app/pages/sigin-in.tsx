5e46a28e0062d9b5f14edd92e24f3bab94280086
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2024-12-30 16:08:20 +0900

    박서현 영어 개못해

--- app/pages/sigin-in.tsx
+++ app/pages/sigin-in.tsx
@@ -1,99 +1,35 @@
 'use client'
 
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import * as S from '../styles/signin-in';
 import { useRouter } from 'next/navigation';
-import axios from 'axios';
 
 function Signin() {
   const router = useRouter(); 
-  const [noticeShow, setNoticeShow] = useState(false);
-  const noticeRef = useRef<HTMLDivElement | null>(null);
-  
-  const [noticeTitle, setNoticeTitle] = useState('');
-  const [noticeBody, setNoticeBody] = useState('');
-  const [savedNotices, setSavedNotices] = useState<savedNotice[]>([]);
-  
-  const [userName, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('/api/register', {
-        userName,
-        password,
-        email,
-      });
-
-      if (response.status === 201) {
-        console.log('Registration successful');
-        router.push('/Sigin2');
-      }
-    } catch (error) {
-      if (error.response) {
-        console.log(`Error: ${error.response.data.message}`);
-      } else {
-        console.error('Error:', error);
-      }
-    }
-  };
-
-
-
-  interface savedNotice {
-    title: string;
-    body: string;
+  function nextSigin() {
+    router.push('/Sigin2');
   }
-
-  useEffect(() => {
-    // 로컬 스토리지에서 공지사항을 가져옴
-    const noticeData = JSON.parse(localStorage.getItem('notices') || '[]');
-    setSavedNotices(noticeData);
-  }, []);
-
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (noticeRef.current && !noticeRef.current.contains(event.target as Node)) {
-        setNoticeShow(false);
-      }
-    }
-    if (noticeShow) {
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside);
-    }
-    return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, [noticeShow]);
 
   return (
     <>
-      <S.Logo src="/logowhite.png" alt="로고" />
-      <S.Background src="/background2.png" alt="배경" />
+      <S.allDiv>
+        <S.Logo src="/logowhite.png" alt="로고" />
+        <S.Background src="/background2.png" alt="배경" />
 
-      <S.LoginWhite>
-        <S.LoginWhiteText>회원가입</S.LoginWhiteText>
-        <form onSubmit={handleSubmit}>
+        <S.sigininWhite>
+          <S.siginWhiteText>회원가입</S.siginWhiteText>
+
           <S.EmailInputText>이메일</S.EmailInputText>
-          <S.EmailInput 
-            type="email" 
-            placeholder="이메일" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-          />
-          <S.PasswordInputText>학번</S.PasswordInputText>
-          <S.PasswordInput 
-            type="password" 
-            placeholder="학번" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-          />
-          <S.LoginButton>
-            <S.LoginButtonText>다음</S.LoginButtonText>
-          </S.LoginButton>
-        </form>
-      </S.LoginWhite>
+          <S.EmailInput type="email" placeholder="이메일"/>
+          <S.classNumberInputText>학번</S.classNumberInputText>
+          <S.classNumberInput type="password" placeholder="학번" />
+
+          <S.nextButton onClick={nextSigin}>
+            <S.nextButtonText>다음</S.nextButtonText>
+          </S.nextButton>
+        </S.sigininWhite>
+      </S.allDiv>
     </>
   );
 }
--- app/styles/signin-in.ts
+++ app/styles/signin-in.ts
@@ -1,104 +1,109 @@
 import styled from "styled-components";
 
-export const ImageSettion = styled.div`
-`;
+export const allDiv = styled.div`
+    position:absolute;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+`
 
 export const Logo = styled.img`
-position:absolute;
-left:120px;  
-top:20px;
+    position:absolute;
+    left:120px;  
+    top:20px;
 `;
 
 export const Background = styled.img`
-height: 100vh;
-width: 100vw;
-zindex:-1;
+    height: 100vh;  // 화면 전체 높이
+    width: 100vw;   // 화면 전체 너비
+    z-index:-1;
+    position:absolute;
 `;
-export const LoginWhite=styled.div`
-width: 400px;
-height: 355px;
-position:absolute;
-left: 560px;
-top: 200px;
-background-color:#ffff;
-border-radius:15px;
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;     
+export const sigininWhite=styled.div`
+    width: 400px;
+    height: 422px;
+    background-color:#ffff;
+    border-radius:15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
-export const LoginWhiteText=styled.span`
-font-family: 'Pretendard';
-font-weight: 700;
-font-size: 24px;
-color: #000000;
-`
-
-export const EmailInput=styled.input`
-width: 334px;
-height: 52px;
-background-color: #f4f4f4;
-color: #000000;
-border-radius: 8px;
-padding:10px;
-outline: none
-`
-
-export const PasswordInput=styled.input`
-width: 334px;
-height: 52px;
-background-color: #f4f4f4;
-color: #000000;
-border-radius: 8px;
-padding:10px;
-outline: none
-margin-top: 40px;
-`
-export const LoginButton=styled.div`
-width: 96px;
-height: 48px;
-margin-top:50px;
-margin-left: 120px;
-background-color: #003C82;
-border-radius:18px;
-display: flex;
-align-items: center;
-justify-content: center;
-cursor: pointer;
-`
-export const LoginButtonText=styled.span`
-font-family: 'Pretendard';
-font-style: normal;
-font-weight: 700;
-font-size: 16px;
-line-height: 19px;
-color: #FAFAFA;
-text-align: center;
-display: flex;
-align-items: center;
-justify-content: center;    
+export const siginWhiteText=styled.span`
+    font-family: 'Pretendard';
+    font-weight: 700;
+    font-size: 24px;
+    color: #000000;
+    margin-top: 24px;
 `
 export const EmailInputText=styled.span`
-font-family: 'Pretendard';
-font-style: normal;
-font-weight: 500;
-font-size: 16px;
-line-height: 17px;
-position: relative;
-display: flex;
-align-items: center;
-margin-top:20px;    
-color: #000000;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 17px;
+    color: #000000;
+    margin-left:-280px;
+    margin-top: 40px;
+    margin-bottom: 10px;
 `
-export const PasswordInputText=styled.span`
-font-family: 'Pretendard';
-font-style: normal;
-font-weight: 500;
-font-size: 16px;
-line-height: 17px;
-display: flex;
-align-items: center;
-color: #000000;
-position: relative;
-margin-top:30px;
+export const EmailInput=styled.input`
+    width: 334px;
+    height: 28px;
+    background-color: #f4f4f4;
+    color: #000000;
+    font-size: 18px;
+    border-radius: 8px;
+    padding:10px;
+    border: none;
+    margin-top: 4px;
+`
+
+export const classNumberInputText=styled.span`
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 17px;
+    color: #000000;
+    margin-right: 300px;
+    margin-top: 40px;
+    margin-bottom: 4px;
+`
+
+export const classNumberInput=styled.input`
+    width: 334px;
+    height: 28px;
+    margin-top: 10px;
+    background-color: #f4f4f4;
+    color: #000000;
+    border-radius: 8px;
+    padding:10px;
+    border: none;
+    font-size: 18px;
+`
+export const nextButton=styled.div`
+    width: 112px;
+    height: 56px;
+    margin-top: 40px;
+    background-color: #003C82;
+    border-radius:18px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const nextButtonText=styled.span`
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 19px;
+    color: #FAFAFA;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
